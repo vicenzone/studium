bef31373bc18a45c5bf4ebfef8b2a7c2dc2c4d6b
vincenzone <vincymail123@gmail.com>
2021-10-14 23:32:18 +0200

    small update

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@ require 'mina/deploy'
 require 'mina/git'
 
 set :domain, '82.84.89.28'
-set :user, 'deploy'
+set :user, 'vincent'
 set :deploy_to, '/var/www/studium'
 set :repository, 'https://vicezone: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@github.com/vicenzone/studium.git'
 set :branch, 'master'
 set :forward_agent, true
-set :keep_releases, 100
+set :keep_releases, 15
 set :execution_mode, :pretty
 
 desc "Deploy to all servers"
@@ -26,8 +26,8 @@ task :deploy do
     on :launch do
       in_path(fetch(:current_path)) do
          invoke :'deploy:cleanup'
-         command 'pm2 delete all -s || true'
-         command %{pm2 start app.js --name studium-demo}
+         command %{pm2 delete all -s || true}
+         command %{pm2 start app.js}
       end
     end
   end
